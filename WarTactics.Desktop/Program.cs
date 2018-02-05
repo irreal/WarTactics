4af89ddef19250b5852dcc51a9cc4216e3abc8de
Miloš Spasojević <mspasojevic@showingtime.com>
2018-02-05 17:20:45 +0100

    Initial commit on WarTactics game

---
+++ WarTactics.Desktop/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace WarTactics.Desktop
+{
+    using System;
+
+    using WarTactics.Shared;
+
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            using (var game = new WtGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
